df4ada2e82f2fc70c9829c8df3033f6b0d277d3a
JacobMorgan3 <144738756+JacobMorgan3@users.noreply.github.com>
2024-01-25 16:50:05 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,26 @@
-function binarySearch(list, element) {
-    return -1;
+function binarysearch (a, b) {
+    var start = 0;
+    var end = a.length;
+    var midpoint = Math.floor(((end - start) / 2) + start);
+
+    if (b >= a[a.length - 1]) {
+        return -1;}
+
+    while (true) {
+        //console.log(midpoint);)
+        if ((end - start) <= 0)
+            return -1;
+
+        midpoint = Math.floor(((end - start) / 2) + start);
+
+        if (b == a[midpoint]) {
+            return midpoint;}
+
+        else if (b < a[midpoint]) {
+            end = midpoint;}
+
+        else if(b > a[midpoint]) {
+           start = midpoint}
+          
+    }      
 }
