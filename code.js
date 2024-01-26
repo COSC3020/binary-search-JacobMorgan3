4b877f60418844571c609df39ac563998a1ae093
Jacob Morgan <jmorga29@uwyo.edu>
2024-01-25 17:18:19 -0700

    testing

--- code.js
+++ code.js
@@ -1,3 +1,5 @@
+var array = [5,6,7,7,9,10,11,12];
+
 function binarySearch (a, b) {
     var start = 0;
     var end = a.length;
@@ -20,7 +22,8 @@ function binarySearch (a, b) {
             end = midpoint;}
 
         else if(b > a[midpoint]) {
-           start = midpoint}
-          
+           start = midpoint + 1}
     }      
 }
+
+console.log(binarySearch(array, 8));
